6d4e826bdc00aa373286217155c7ce7a0ea18ad0
Trai <51330621+Traijan1@users.noreply.github.com>
2021-11-30 11:51:29 +0100

    added shortcut (ctrl + .) for editing json in vscode

--- scripts/index.js
+++ scripts/index.js
@@ -4,3 +4,10 @@ var vue = new Vue({
         linkList: linkList
     }
 });
+
+// If you host it locally and want to change json with vscode:
+window.addEventListener("keydown", event => {
+    if(event.key == "." && event.ctrlKey) {
+        window.open("vscode://file/C:/Users/aweber/Documents/data/links.js", '_blank').focus();
+    }
+})
